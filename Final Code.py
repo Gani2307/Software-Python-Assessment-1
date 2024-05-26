2bc1858ee26e07b8bd609a2bc7f6eeb0aa9197a3
Gani2307 <abhishek.ganimatam@education.nsw.gov.au>
2024-05-26 17:52:16 +1000

    Start Quiz
    Started the quiz page with the questions

--- Final Code.py
+++ Final Code.py
@@ -1,19 +1,13 @@
 import tkinter as Tk
 from PIL import Image, ImageTk
 
-root=Tk.Tk()
-root.title("Example of a button")
-root.geometry("800x800")
-root.configure(background="IndianRed1")
-Label1 = Tk.Label(root,text="WORLD WAR II",
-               font=("Impact", 50),
-               bg= "IndianRed1")
-Label1.pack(pady = 20)
-Label1.place(x=1000, y=100)
+
+global root
 
 def Main_page():
+    global root
     root=Tk.Tk()
-    root.title("Example of a button")
+    root.title("WORLD WAR II")
     root.geometry("800x800")
     root.configure(background="IndianRed1")
     
@@ -32,11 +26,9 @@ def Main_page():
    
 
     def Start_Quiz():
-        Questions = Tk.Label(root)
-        Questions.pack()
-
+        root.destroy()
+        Start_Quiz_Page()
         
-    
     Start_Quiz_button = Tk.Button(root,text="Start Quiz", bg="lightgreen", font=("Arial", 30), command=Start_Quiz)
     Start_Quiz_button.place(x=100,y=400)
    
@@ -44,19 +36,32 @@ def Main_page():
     button3 = Tk.Button(root, text="Exit", height=3, width=15, bg='gray20', fg='white', relief="raised", command=root.destroy)
     button3.place(x=455, y=410)
 
-
     root.mainloop()
-
-
-Information = Tk.Tk()
-Information.title("Overview")
-Information.geometry("800x800")
 
 def Overview_Page():
     Information = Tk.Tk()
-    Information.title("Overview")
+    Information.title("WORLD WAR II")
     Information.geometry("800x800")
+    Information.configure(background="IndianRed1")
     
-    
-    
+    def Back_Page():
+        Information.destroy()
+        Main_page()
+
+        
+    Back_button = Tk.Button(Information, text = "Home", bg="lightgreen", font=("Arial", 15), command=Back_Page)
+    Back_button.pack()
+
     Information.mainloop()
+
+def Start_Quiz_Page():
+    Question = Tk.Tk()
+    Question.title("WORLD WAR II")
+    Question.geometry("800x800")
+    Question.configure(background="IndianRed1") 
+
+
+
+
+
+Main_page()
