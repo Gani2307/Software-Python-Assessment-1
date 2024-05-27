f3facad77697dc175a2c8b0b23f92e4d45cbb675
Gani2307 <abhishek.ganimatam@education.nsw.gov.au>
2024-05-27 18:44:22 +1000

    Getting Rid of customtkinter
    Got rid of customtkinter as invalid command name error would pop up

--- Final Code.py
+++ Final Code.py
@@ -1,63 +1,71 @@
-import customtkinter as Tk
+import tkinter as tk
 from PIL import Image, ImageTk
 
 
-global root
-
+# Main Page code
 def Main_page():
-    root=Tk.CTk()
-    root.title("WORLD WAR II")
-    root.geometry("1000x700")
-    root.configure(background="Khaki3")
-    root.resizable(False, False)
+    global homepage
+    homepage=tk.Tk()
+    homepage.title("WORLD WAR II")
+    homepage.geometry("1000x700")
+    homepage.configure(background="Khaki3")
+    homepage.resizable(False, False)
     
-    Label1 = Tk.CTkLabel(root,text="WORLD WAR II", font=("Impact", 50), bg_color= "Khaki3")
-    Label1.pack()
-    Label1.place(x=490, y=50)
+    Title =tk.Label(homepage,text="WORLD WAR II", font=("Impact", 50), bg = "Khaki3")
+    Title.pack()
+    Title.place(x=490, y=50)
 
+
+
+# Code for Button Functions
     def Overview():
-        root.destroy()
-        Overview_Page()
+        homepage.destroy()
+        Open_Overview()
 
-    Overview_button = Tk.CTkButton(root,text="Overview", bg_color="lightgreen", font=("Arial", 30), command = Overview)
+    def Quiz():
+        homepage.destroy()
+        Open_Quiz()
+
+
+    # Code for Buttons
+    Overview_button = tk.Button(homepage,text="Overview", bg ="lightgreen", font=("Arial", 30), command = Overview)
     Overview_button.place(x=100,y=500)
-   
-
-    def Start_Quiz():
-        root.destroy()
-        Start_Quiz_Page()
-        
-    Start_Quiz_button = Tk.CTkButton(root,text="Start Quiz", bg_color="lightgreen", font=("Arial", 30), command=Start_Quiz)
+    
+    Start_Quiz_button = tk.Button(homepage,text="Start Quiz", bg ="lightgreen", font=("Arial", 30), command=Quiz)
     Start_Quiz_button.place(x=100,y=400)
    
 
-    button3 = Tk.CTkButton(root, text="Exit", height=3, width=15, bg_color='gray20', fg_color='white', command=root.destroy)
+    button3 = tk.Button(homepage, text="Exit", height=3, width=15, bg ='gray20', fg ='white', command=homepage.destroy)
     button3.place(x=455, y=410)
 
-    root.mainloop()
-
-def Overview_Page():
-    Information = Tk.CTk()
-    Information.title("WORLD WAR II")
-    Information.geometry("800x800")
-    Information.configure(background="IndianRed1")
+    homepage.mainloop()
     
-    def Back_Page():
-        Information.destroy()
-        Main_page()
 
-        
-    Back_button = Tk.CTkButton(Information, text = "Home", bg_color="lightgreen", font=("Arial", 15), command=Back_Page)
-    Back_button.pack()
-
-    Information.mainloop()
-
-def Start_Quiz_Page():
-    Question = Tk.CTk()
+def Open_Quiz ():
+    Question = tk.Tk()
     Question.title("WORLD WAR II")
     Question.geometry("800x800")
     Question.configure(background="IndianRed1") 
     Question.resizable(False, False)
 
 
-Main_page()
+def Open_Overview ():
+    Information = tk.Tk()
+    Information.title("WORLD WAR II")
+    Information.geometry("800x800")
+    Information.configure(background="IndianRed1")
+    Information.resizable(False, False)
+
+    def Back_Page ():
+        Information.destroy()
+        Main_page()
+
+        
+    Back_button = tk.Button(Information, text = "Home", bg ="lightgreen", font=("Arial", 15), command=Back_Page)
+    Back_button.pack()
+
+    Information.mainloop()
+
+
+
+Main_page()     
