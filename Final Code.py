abf256917d60dd9dc457127fa7877ee27cf2f895
Gani2307 <abhishek.ganimatam@education.nsw.gov.au>
2024-05-28 22:26:27 +1000

    Started Quiz
    Started the quiz function

--- Final Code.py
+++ Final Code.py
@@ -1,6 +1,48 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+current_score_index = 0
+score = 0
+
+Questions = [
+
+    {
+        "Question": "What year did World War II start?",
+        "options": ["1939", "1941", "1942", "1945"],
+        "answer": "1939"
+    },
+    {
+        "Question": "When did World War II end?",
+        "options": ["1945", "1946", "1947", "1948"],
+        "answer": "1945"
+    },
+    {
+        "Question": "Which country was not part of the Axis Powers in World War II?",
+        "options": ["Germany", "Japan", "Italy", "France"],
+        "answer": "France"
+    },
+    {
+        "question": "Who was the Prime Minister of the United Kingdom during most of World War II?",
+        "options": ["Winston Churchill", "Neville Chamberlain", "Clement Attlee", "Stanley Baldwin"],
+        "answer": "Winston Churchill"
+    },
+    {
+        "Question": "Which event led to the United States entering World War II?",
+        "options": ["Attack on Pearl Harbor", "Battle of Stalingrad", "D-Day", "Bombing of Hiroshima"],
+        "answer": "Attack on Pearl Harbor"
+    },
+    {
+        "Question": "What year did World War II start?",
+        "options": ["1939", "1941", "1942", "1945"],
+        "answer": "1939"
+    },
+    {
+        "Question": "When did World War II end?",
+        "options": ["1945", "1946", "1947", "1948"],
+        "answer": "1945"
+    }
+]
+
 
 # Main Page code
 def Main_page():
@@ -41,30 +83,56 @@ def Main_page():
     homepage.mainloop()
     
 
-def Open_Quiz ():
-    Question = tk.Tk()
-    Question.title("WORLD WAR II")
-    Question.geometry("800x800")
-    Question.configure(background="IndianRed1") 
-    Question.resizable(False, False)
-
 
 def Open_Overview ():
     Information = tk.Tk()
     Information.title("WORLD WAR II")
-    Information.geometry("800x800")
+    Information.geometry("1000x700")
     Information.configure(background="IndianRed1")
     Information.resizable(False, False)
+
+
 
     def Back_Page ():
         Information.destroy()
         Main_page()
 
-        
-    Back_button = tk.Button(Information, text = "Home", bg ="lightgreen", font=("Arial", 15), command=Back_Page)
-    Back_button.pack()
+
+
+    Back_button = tk.Button(Information, text = "Back", bg ="lightgreen", font=("Arial", 15), command=Back_Page)
+    Back_button.place(x=100, y=400)
 
     Information.mainloop()
+
+selected_option = None
+
+
+
+def Open_Quiz ():
+    global current_score_index, score
+    current_score_index = 0
+    score = 0
+    Question = tk.Tk()
+    Question.title("WORLD WAR II")
+    Question.geometry("1000x700")
+    Question.configure(background="IndianRed1") 
+    Question.resizable(False, False)
+
+    def Check_Answer(selected_option):
+        global current_question_index, score
+        print(current_question_index)
+        Correct_Answer = Questions[current_question_index]["Answer"]
+
+    def Back_Page ():
+        Question.destroy()
+        Main_page()
+
+    Back_button = tk.Button(Question, text = "Back", bg ="lightgreen", font=("Arial", 15), command=Back_Page)
+    Back_button.pack()
+
+ 
+
+
 
 
 
